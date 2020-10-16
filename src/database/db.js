ad379c902de290bcae9480847afc38375c8833f1
Gybson Klarck <68571007+SunnyUwU@users.noreply.github.com>
2020-10-16 13:24:37 -0300

    Test database

---
+++ src/database/db.js
@@ -0,0 +1,21 @@
+//importar dependências
+const database = require('sqlite-async');
+
+database.open(__dirname + '/database.sqlite').then(execute)
+
+function execute(db) {
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS orphanages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            lat TEXT,
+            lng TEXT,
+            name TEXT,
+            about TEXT,
+            whatsapp TEXT,
+            images TEXT,
+            instructions TEXT,
+            opening_hours TEXT,
+            open_on_weekends TEXT
+        );
+    `)
+}
