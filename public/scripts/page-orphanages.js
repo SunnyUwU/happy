4740cadf95b9c29220da03f58208ddd44965061c
Gybson Klarck <68571007+SunnyUwU@users.noreply.github.com>
2020-10-15 21:44:11 -0300

    Errors correction

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -4,7 +4,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 //create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "../../public/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
@@ -16,7 +16,7 @@ const popup = L.popup({
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das Meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" > </a>')
+}).setContent('Lar das Meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="../../public/images/arrow-white.svg" > </a>')
 
 //create marker
 L.marker([-8.0544383, -34.9100154], {icon})
