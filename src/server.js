0740dd03fa19115c65d756de95114b84981f5bdd
Gybson Klarck <68571007+SunnyUwU@users.noreply.github.com>
2020-10-15 23:33:57 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log("Roi");
+//importar dependência
+const express = require('express');
+const path = require('path')
+
+//iniciando o express
+const server = express()
+server
+//utilizando arquivos estáticos
+.use(express.static('public'))
+//criar rota
+.get('/', (request, response) => {
+    console.log(request.query)
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+//ligar o server
+server.listen(5500)
