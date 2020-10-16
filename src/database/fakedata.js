ccc548f42a0e493ff0455dfc8115a23e91c543a2
Gybson Klarck <68571007+SunnyUwU@users.noreply.github.com>
2020-10-16 10:15:41 -0300

    Create fakedata.js

---
+++ src/database/fakedata.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        id: 1, 
+        lat: "-8.0544383",
+        lng:  "-34.9100154",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.", 
+        images: [
+            "https://images.unsplash.com/photo-1598983290606-cf5876b08655?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1597992350431-56cb7e28a7a2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ], 
+        instructions: "Venha conhecer e traga muito amor e paciência.",
+        opening_hours: "Horário de visitas Das 8h as 18h",
+        open_on_weekends: "1"
+
+    },
+
+    {
+        id: 2, 
+        lat: "-8.0544383",
+        lng:  "-34.9200154",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.", 
+        images: [
+            "https://images.unsplash.com/photo-1598983290606-cf5876b08655?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1597992350431-56cb7e28a7a2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ], 
+        instructions: "Venha conhecer e traga muito amor e paciência.",
+        opening_hours: "Horário de visitas Das 8h as 18h",
+        open_on_weekends: "0"
+    }
+]
